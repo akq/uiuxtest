6df24482c0ae3f0cdeda914b6bcb3637f9a85553
Min <a.b@c.com>
2022-05-25 21:41:34 +0800

    fix dir issue after publishing

--- config/jest/babelTransform.js
+++ config/jest/babelTransform.js
@@ -2,7 +2,12 @@
 var path = require('path')
 
 var babelJest = require('babel-jest').default;
-var ctx = path.join(__dirname, '../../node_modules')
+var ctx = path.join(
+  __dirname, 
+  ~__dirname.indexOf('node_modules'+path.sep)
+  ? '../../..'
+  : '../../node_modules'
+)
 module.exports = babelJest.createTransformer({
   presets: [
     path.join(ctx, 'babel-preset-react-app'), path.join(ctx,'@babel/preset-react')
